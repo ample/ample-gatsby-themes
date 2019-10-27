25dd61eed79e6cfc374b7235d116ce738018fb4b
Sean C Davis <scdavis41@gmail.com>
2019-10-27 07:10:55 -0400

    Setup docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -20,4 +20,7 @@ window.___navigate = pathname => {
 }
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module)
+configure(
+  require.context('../stories', true, /\.stories\.(js|jsx|ts|tsx|mdx)$/),
+  module
+)
