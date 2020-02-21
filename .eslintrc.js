7f036db2b29e76ad633fa1751fd6b0db91cb6545
Sean C Davis <scdavis41@gmail.com>
2020-02-21 13:04:07 -0500

    Whitelist jest vars for eslint Instead of jest/globals (it was causing problems in a gatsby project)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     browser: true,
-    "jest/globals": true
+    jest: true
   },
   extends: ["eslint:recommended", "plugin:react/recommended", "plugin:jsx-a11y/recommended"],
   globals: {
