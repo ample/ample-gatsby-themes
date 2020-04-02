57bba73cfb4da97e9548240380b94a759f7ac73f
Jeff Lay <phritolay@icloud.com>
2020-04-02 14:56:59 -0400

    add config and script for stylelint
    resolves #13

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,35 @@
 module.exports = {
   env: {
+    es6: true,
     browser: true,
+    node: true,
     jest: true
   },
   extends: ["eslint:recommended", "plugin:react/recommended", "plugin:jsx-a11y/recommended"],
   globals: {
     __PATH_PREFIX__: true
   },
-  plugins: ["prettier", "jsx-a11y", "jest"],
+  overrides: [
+    {
+      files: ["**/*.js"]
+    },
+    {
+      files: ["./__mocks__/*.js"],
+      rules: {
+        "no-unused-vars": 0
+      }
+    }
+  ],
   parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     }
   },
+  plugins: ["prettier", "jsx-a11y", "jest"],
   rules: {
-    "react/no-unescaped-entities": "off"
+    "react/no-unescaped-entities": "off",
+    "react/prop-types": [2, { ignore: ["className"] }]
   },
   settings: {
     react: {
