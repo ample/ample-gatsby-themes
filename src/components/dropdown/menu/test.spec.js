37ab63ba59ce6e146a78124dd5e0b99fd1d77da5
Sean C Davis <scdavis41@gmail.com>
2020-05-13 14:55:23 -0400

    Fix specs

--- src/components/dropdown/menu/test.spec.js
+++ src/components/dropdown/menu/test.spec.js
@@ -7,7 +7,7 @@ import fixture from "./__fixtures__/dropdown_menu.json"
 
 describe("DropdownMenu", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<DropdownMenu items={fixture.items} />).toJSON()
+    const tree = renderer.create(<DropdownMenu items={fixture} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
