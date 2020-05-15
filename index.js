9728a7d5b7fd3a74ed96033eb5c03f58925be543
Sean C Davis <scdavis41@gmail.com>
2020-05-15 05:41:43 -0400

    Export Icon component

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import Button from "./src/components/button"
 import DebugMediaQueries from "./src/components/debug-media-queries"
+import Icon from "./src/components/icon"
 import Image from "./src/components/image"
 import Link from "./src/components/link"
 import LinkList from "./src/components/link-list"
 import SEO from "./src/components/seo"
 
-export { Button, DebugMediaQueries, Link, LinkList, SEO, Image }
+export { Button, DebugMediaQueries, Icon, Image, Link, LinkList, SEO }
